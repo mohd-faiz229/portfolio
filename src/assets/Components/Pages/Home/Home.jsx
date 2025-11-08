7e18ebbf727098e95d403b35dc8d944f89056dba
mohd faiz <faizkhan94129876@gmail.com>
2025-11-08 17:38:53 +0530

    commit finalized

--- src/assets/Components/Pages/Home/Home.jsx
+++ src/assets/Components/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"; // <-- import Link
-import pfimage from "../../../../assets/images/pfimage.png";
+import pfimage from "../../../images/pfimage.png";
 
 export default function Home() {
     return (
